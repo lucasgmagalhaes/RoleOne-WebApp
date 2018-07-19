8df0bb550a47d3fb3af46fae6ab47bdcfe0685e6
Lucas Gomes <lucasgsm88@gmail.com>
2018-07-18 22:49:25 -0300

    Fixed database service methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,21 @@
 import { Component } from "@angular/core";
 import { FireService } from "./core/services/fire.service";
-
+import { AngularFireList  } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  list: any[];
+  list: Observable<any[]>;
 
-  constructor(private fire: FireService) {}
-
-  public criar() {
-    let person = {
-      name: "teste2"
-    };
-
-    let persons = [
-      {
-        name: "Lucas",
-        age: "12"
-      },
-      {
-        name: "BTATA",
-        age: "12"
-      }
-    ];
-    this.fire.create(person, "tests");
+  constructor(private fire: FireService) {
+    this.fire.delete('-LHTtnZUIFCtGhATGIYg');
+    
   }
+}
+
+export interface test {
+  name: string
 }
--- src/app/core/services/fire.service.ts
+++ src/app/core/services/fire.service.ts
@@ -5,39 +5,43 @@ import {
   AngularFireList
 } from "angularfire2/database";
 import { database } from "firebase";
+import { map } from "rxjs/operators";
+import { Observable } from "rxjs";
 
 /**
  * @class FireService
  * @see angularfire2
- * @description This class is a extention of angularFire2 AngularFireDatabasemethods.
- * It provides all services that the own angularFire2 does for a CRUD implementation,
- * but provides a reduction of used code for the application. For all methods, their
+ * @private AngularFireDatabase
+ * @description This class is a abstraction of angularFire2 AngularFireDatabasemethods.
+ * It provkeyes all services that the own angularFire2 does for a CRUD implementation,
+ * but provkeyes a reduction of used code for the application. For all methods, their
  * return are the same as the primary methods. As the own encapsulation says, this
- * services provides firebase functions only.
+ * services provkeyes firebase functions only.
  */
 @Injectable({
   providedIn: "root"
 })
-export class FireService extends AngularFireDatabase {
-  super() {}
+export class FireService {
+  constructor(private db: AngularFireDatabase) {}
 
   /**
-   * Persists the object into the database without set the generated id in the objects
+   * Persists the object into the database without set the generated key in the objects
    *
    * @param object entity to be persisted
    * @param route path in the database to the entity be persisted
    * @return response of the database(database.ThenableReference)
+   * @throws Error if object or route be undefined
    */
   create(object: Object | Object[], route: string): database.ThenableReference {
     if (object !== undefined && route != undefined) {
       let objSingleReturn: database.ThenableReference;
 
       if (object instanceof Array) {
-        let db = super.list(route);
+        let db = this.db.list(route);
         object.forEach(obj => (objSingleReturn = db.push(obj)));
         return objSingleReturn;
       } else {
-        return super.list(route).push(object);
+        return this.db.list(route).push(object);
       }
     } else {
       throw new Error("Parameters object and route can not be undefined");
@@ -45,16 +49,22 @@ export class FireService extends AngularFireDatabase {
   }
 
   /**
-   * Persists a object into the database setting the his Id value. If the object does not have
+   * Persists a object into the database setting the his key value. If the object does not have
    * this propertie, it is created with writable equals to false
    *
    * @param object entity to be persisted
    * @param route path in the database to the entity be persisted
    * @return response of the database(database.ThenableReference)
+   * @throws Error if object already have an key.
+   * if propertie 'key' of object isn't defined
+   * if object/ object[] or route is undefined
    */
-  createWithKey(object: Object | Object[], route: string): database.ThenableReference {
+  createWithKey(
+    object: Object | Object[],
+    route: string
+  ): database.ThenableReference {
     if (object !== undefined && route !== undefined) {
-      let db = super.list(route);
+      let db = this.db.list(route);
       let objReturn: database.ThenableReference;
 
       if (object instanceof Array) {
@@ -67,7 +77,7 @@ export class FireService extends AngularFireDatabase {
               throw new Error("Object is already registred");
             }
           } else {
-            throw new Error("Obj has no propertie 'key' defined");
+            throw new Error("Obj has no propertie key defined");
           }
         });
 
@@ -80,7 +90,7 @@ export class FireService extends AngularFireDatabase {
             throw new Error("Object is already registred");
           }
         } else {
-          throw new Error("Obj has no propertie 'key' defined");
+          throw new Error("Obj has no propertie key defined");
         }
 
         return objReturn;
@@ -96,12 +106,40 @@ export class FireService extends AngularFireDatabase {
    * @param route path to the entites
    * @param query no required. Used to filter the entities
    * @return list of entites(Observable[])
+   * @throws Error if route is undefined
+   * @type T
    */
   find<T>(route: string, query?: QueryFn): AngularFireList<T> {
     if (route !== undefined) {
-      return super.list(route, query);
+      return this.db.list<T>(route, query);
     } else {
       throw new Error("Propertie route can not be undefined");
+    }
+  }
+
+  /**
+   * Return a list of data with their respective key.
+   *
+   * @param route Path to the data
+   * @param query Not required. Used to filter the data
+   * @return list of entites(Observable[])
+   * @throws Error if route is undefined
+   */
+  findWithKey(route: string, query?: QueryFn): Observable<any[]> {
+    if (route !== undefined) {
+      return this.db
+        .list(route, query)
+        .snapshotChanges()
+        .pipe(
+          map(changes => {
+            return changes.map(c => ({
+              key: c.payload.key,
+              ...c.payload.val()
+            }));
+          })
+        );
+    } else {
+      throw new Error("Parameter route can not be undefined");
     }
   }
 
@@ -109,8 +147,11 @@ export class FireService extends AngularFireDatabase {
    * Update a object in the database based in his key. the object in parameter will replace values of
    * the entity properties or add more.
    *
-   * @param object
-   * @param route
+   * @param object Object to be updated
+   * @param route Path to the collection of the data
+   * @return Promisse of the update
+   * @throws Error if object or some object of list of objects has no propertie 'key'.
+   * if route is undefined.
    */
   update(object: Object | Object[], route: string): Promise<void> {
     if (object.hasOwnProperty("key") && object["key"] !== undefined) {
@@ -119,24 +160,30 @@ export class FireService extends AngularFireDatabase {
 
         object.forEach((obj, index) => {
           if (obj["key"] !== undefined) {
-            //If an object have no Id, so it can not be updated
-            promiseReturn = super.object(`${route}/${obj["key"]}`).update(obj);
+            let key = obj["key"];
+            delete obj["key"];
+
+            promiseReturn = this.db.object(`${route}/${key}`).update(obj);
           } else {
-            throw new Error("Object at index" + index + "has no id");
+            //If an object have no key, so it can not be updated
+            throw new Error("Object at index" + index + "has no key");
           }
         });
 
         return promiseReturn;
       } else {
         if (object["key"] !== undefined) {
-          return super.object(`${route}/${object["key"]}`).update(object);
+          let key = object["key"];
+          delete object["key"];
+
+          return this.db.object(`${route}/${key}`).update(object);
         } else {
-          throw new Error("Object has no id");
+          throw new Error("Object has no key");
         }
       }
     } else {
       throw new Error(
-        "Parameter 'key' not defined for entity to be updated:" + object
+        "Parameter key not defined for entity to be updated:" + object
       );
     }
   }
@@ -151,6 +198,8 @@ export class FireService extends AngularFireDatabase {
    * @param object entitie to be replaced
    * @param route path to the list of entities
    * @return promisse of the update
+   * @throws Error if undefined for object of one object of the list.
+   * <p>if the object/objects[] or route if undefined</p>
    */
   set(object: Object | Object[], route: string): Promise<void> {
     if (object !== undefined && route !== undefined) {
@@ -159,10 +208,13 @@ export class FireService extends AngularFireDatabase {
 
         object.forEach((obj, index) => {
           if (obj["key"] !== undefined) {
-            promiseReturn = super.object(`${route}/${obj["key"]}`).set(object);
+            let key = obj["key"];
+            delete obj["key"];
+
+            promiseReturn = this.db.object(`${route}/${key}`).set(obj);
           } else {
             throw new Error(
-              "Propertie 'key' is undefined for object at index" + index
+              "Propertie key is undefined for object at index" + index
             );
           }
         });
@@ -170,9 +222,12 @@ export class FireService extends AngularFireDatabase {
         return promiseReturn;
       } else {
         if (object["key"] !== undefined) {
-          return super.object(`${route}/${object["key"]}`).set(object);
+          let key = object["key"];
+          delete object["key"];
+
+          return this.db.object(`${route}/${key}`).set(object);
         } else {
-          throw new Error("Propertie 'key' is undefined");
+          throw new Error("Propertie key is undefined");
         }
       }
     } else {
@@ -186,18 +241,40 @@ export class FireService extends AngularFireDatabase {
    *
    * @param route Path to the entities
    * @param key Not required. Removes a specific object or list of objects
+   * @returns Promisse of the deletion
+   * @throws Error if route be undefined
    */
-  delete(route: string, key?: string | string[]): Promise<void> {
+  delete(route: string, key?: string | string[] | object | object[]): any {
     if (route !== undefined) {
-      let db = super.list(route);
+      let db = this.db.list(route);
       let _return: Promise<void>;
 
       if (key !== undefined) {
         if (key instanceof Array) {
-          key.forEach(k => (_return = db.remove(k)));
+          /*
+          * For avoid cast compatibility exception
+          * the array of 'any' is converted to array of string
+          */
+          (key as string[]).forEach((k, i) => {
+            if (typeof k === "string") {
+              _return = db.remove(`/${route}/${k}`);
+            } else if (typeof k === "object") {
+              if (k["key"] !== undefined) {
+                _return = db.remove(`/${route}/${k["key"]}`);
+              } else {
+                throw new Error('Key not defined for element at index: ' + i);
+              }
+            }
+          });
           return _return;
         } else {
-          return db.remove(key);
+          if (typeof key === "string") {
+            return db.remove(key);
+          } else  if (key["key"] !== undefined) {
+            _return = db.remove(`/${route}/${key["key"]}`);
+          } else {
+            throw new Error('Key not defined for object');
+          }
         }
       } else {
         return db.remove();
