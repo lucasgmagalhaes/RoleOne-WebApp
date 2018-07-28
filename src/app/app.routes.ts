860cdf961b4130fdb6dc745df717521040f57c1a
Lucas Gomes <lucasgsm88@gmail.com>
2018-07-28 18:38:02 -0300

    Created login template. relationated with:  #1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from "@angular/router";
 import { RegisterComponent } from "./register/register.component";
-
-export const ROUTES: Routes = [{ path: "", component: RegisterComponent }];
+import { LoginComponent } from "./login/login.component";
+export const ROUTES: Routes = [
+  { path: "", component: RegisterComponent },
+  { path: "login", component: LoginComponent }
+];
