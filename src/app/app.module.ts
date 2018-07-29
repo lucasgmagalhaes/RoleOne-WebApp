62109e2d9f0a480c3af34b9082fe402c5cd62668
Lucas Gomes <lucasgsm88@gmail.com>
2018-07-28 23:28:43 -0300

    Created login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
 import { HeaderComponent } from "./header/header.component";
-import { SingupComponent } from "./singup/singup.component";
 import { RouterModule } from "@angular/router";
 import { ROUTES } from "./app.routes";
 import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, SingupComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, LoginComponent, RegisterComponent],
   imports: [BrowserModule, CoreModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
