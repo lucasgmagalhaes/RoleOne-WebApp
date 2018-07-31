4ca42cce1cabd2c28a2dbd56b906660e7666ce58
Lucas Gomes <lucasgsm88@gmail.com>
2018-07-31 15:53:36 -0300

    Making random names for login page #34

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'ro1-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "ro1-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
+  private greatings: Array<string> = [
+    "Mage",
+    "Warrior",
+    "White Mage",
+    "Rogue",
+    "Murlock",
+    "Goblin",
+    "to the republic"
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getRandomGreeting(): string {
+    let guy = this.greatings[
+      Math.floor(Math.random() * this.greatings.length + 0)
+    ];
+    //console.log(this.greatings.indexOf(guy));
+    return guy;
   }
-
 }
