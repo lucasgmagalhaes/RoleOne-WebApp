37e5adcd15110ebf2a79cf341783667b21d509c2
Lucas Gomes <lucasgsm88@gmail.com>
2018-07-31 14:37:49 -0300

    Creatad home component #33 and his route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,8 +9,9 @@ import { FooterComponent } from './static/footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { HomeComponent } from './home/home.component';
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, LoginComponent, RegisterComponent, ForgotPasswordComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, LoginComponent, RegisterComponent, ForgotPasswordComponent, HomeComponent],
   imports: [BrowserModule, CoreModule, RouterModule.forRoot(ROUTES)],
   providers: [],
   bootstrap: [AppComponent]
