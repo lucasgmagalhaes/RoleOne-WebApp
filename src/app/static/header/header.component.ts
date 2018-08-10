25eb7cc25dbaf9e4dd82d3e4be09a8158b419f83
lgmagalhaes <lucasgsm88@gmail.com>
2018-08-10 15:58:21 -0300

    adjusted header and created tooltip for campain itens

--- src/app/static/header/header.component.ts
+++ src/app/static/header/header.component.ts
@@ -11,13 +11,34 @@ export class HeaderComponent implements OnInit {
   userLogged: boolean = false;
   constructor(private auth: AuthService, private route: Router) {}
   ngOnInit() {
-    //Creates a event for nav burger display
-    let navbar: HTMLElement = document.querySelector(".navbar-burger");
-    navbar.addEventListener("click", function() {
-      let target = navbar.dataset.target;
-      let $target = document.getElementById(target);
-      navbar.classList.toggle("is-active");
-      $target.classList.toggle("is-active");
+    this.createNavBarEvent();
+  }
+
+  /**
+   * Method from Bulma.io
+   * @link https://bulma.io/documentation/components/navbar/
+   */
+  createNavBarEvent(): void {
+    document.addEventListener("DOMContentLoaded", () => {
+      // Get all "navbar-burger" elements
+      const $navbarBurgers = Array.prototype.slice.call(
+        document.querySelectorAll(".navbar-burger"),
+        0
+      );
+      // Check if there are any navbar burgers
+      if ($navbarBurgers.length > 0) {
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+          el.addEventListener("click", () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle("is-active");
+            $target.classList.toggle("is-active");
+          });
+        });
+      }
     });
   }
 
