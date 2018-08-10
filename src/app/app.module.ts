512dd96da3b3b9f3456f6293fd905661063e81e3
lgmagalhaes <lucasgsm88@gmail.com>
2018-08-10 17:55:22 -0300

    module for static components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { CoreModule } from "./core/core.module";
-import { HeaderComponent } from "./static/header/header.component";
 import { appRoutesModule } from "./app.routes";
-import { FooterComponent } from "./static/footer/footer.component";
 import { APP_BASE_HREF } from "@angular/common";
 import { AuthModule } from "./auth/auth.module";
 import { HomeContainerModule } from "./home-container/home-container.module";
+import { StaticModule } from "./static/static.module";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     CoreModule,
     AuthModule,
     appRoutesModule,
-    HomeContainerModule
+    HomeContainerModule,
+    StaticModule
   ],
   providers: [],
   bootstrap: [AppComponent]
