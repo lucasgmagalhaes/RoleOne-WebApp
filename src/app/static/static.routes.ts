0589fe7af968bdbb05b117b908bf0f6c02f470dc
Lucas Gomes <lucasgsm88@gmail.com>
2018-08-11 02:22:01 -0300

    creating profile

--- src/app/static/static.routes.ts
+++ src/app/static/static.routes.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from "@angular/router";
 import { ProfileEditionComponent } from "../detail/components/profile-edition/profile-edition.component";
+import { ProfileComponent } from "../detail/components/profile/profile.component";
 
 export const ROUTES: Routes = [
+  { path: "profile", component: ProfileComponent },
   { path: "profileEdition", component: ProfileEditionComponent }
 ];
 
