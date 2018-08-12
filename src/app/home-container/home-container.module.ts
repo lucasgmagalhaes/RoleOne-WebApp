736b825ed5adddb5d85347525ace4a59970f48e7
Lucas Gomes <lucasgsm88@gmail.com>
2018-08-12 13:35:29 -0300

    adjusted font-awesome dependency and routes

--- src/app/home-container/home-container.module.ts
+++ src/app/home-container/home-container.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './components/home/home.component';
-import { CampainsComponent } from './components/campains/campains.component';
-import { CampainComponent } from './components/campains/campain/campain.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HomeComponent } from "./components/home/home.component";
+import { CampainsComponent } from "./components/campains/campains.component";
+import { CampainComponent } from "./components/campains/campain/campain.component";
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [HomeComponent, CampainsComponent, CampainComponent]
+  imports: [CommonModule],
+  declarations: [HomeComponent, CampainsComponent, CampainComponent],
+  exports: [HomeComponent, CampainsComponent, CampainComponent]
 })
-export class HomeContainerModule { }
+export class HomeContainerModule {}
--- src/app/static/static.routes.ts
+++ src/app/static/static.routes.ts
@@ -1,10 +1,8 @@
 import { Routes, RouterModule } from "@angular/router";
-import { ProfileEditionComponent } from "../detail/components/profile-edition/profile-edition.component";
 import { ProfileComponent } from "../detail/components/profile/profile.component";
 
 export const ROUTES: Routes = [
-  { path: "profile", component: ProfileComponent },
-  { path: "profileEdition", component: ProfileEditionComponent }
+  { path: "profile", component: ProfileComponent }
 ];
 
 export const StaticRoutesModule = RouterModule.forChild(ROUTES);
