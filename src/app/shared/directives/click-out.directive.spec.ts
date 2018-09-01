790b6d16c878d1ae02b5ab0570efb25d1b74eae3
lgmagalhaes <lucasgsm88@gmail.com>
2018-09-01 10:12:50 -0300

    fixed unitary test of clickout directive

--- src/app/shared/directives/click-out.directive.spec.ts
+++ src/app/shared/directives/click-out.directive.spec.ts
@@ -1,9 +1,10 @@
-import { ClickOutDirective } from './click-out.directive';
-import { ElementRef, asNativeElements } from '@angular/core';
+import { ClickOutDirective } from "./click-out.directive";
+import { ElementRef } from "@angular/core";
 
-describe('ClickOutDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickOutDirective();
+describe("ClickOutDirective", () => {
+  it("should create an instance", () => {
+    let element = new ElementRef("<div>BLA</div>");
+    const directive = new ClickOutDirective(element);
     expect(directive).toBeTruthy();
   });
 });
