77badaeb8470922d0bed772d840f59d7ec26588e
Lucas Gomes <lucasgsm88@gmail.com>
2018-09-02 00:52:35 -0300

    finished theme selection template( relationated with campaign creation #65)

--- src/app/game/components/theme-modal/theme-modal.component.ts
+++ src/app/game/components/theme-modal/theme-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
 import { ModalService } from "../../services/modal.service";
+import { Theme } from "../../models/theme.interface";
 
 @Component({
   selector: "ro1-theme-modal",
@@ -9,12 +10,38 @@ import { ModalService } from "../../services/modal.service";
 })
 export class ThemeModalComponent implements OnInit {
   activate: boolean;
-
+  selectedTheme: Theme;
+  themes: Theme[] = [
+    {
+      name: "d&d",
+      description:
+        "Lorem ipsum hendrerit dictumst taciti rhoncus vulputate faucibus rhoncus, proin ut suspendisse nunc" +
+        "nisi amet nunc ullamcorper, suspendisse condimentum sodales quisque congue luctus etiam. dui augue in proin habitasse" +
+        "faucibus, lectus consectetur eros fringilla inceptos suscipit, eget sollicitudin vitae facilisis. egestas quis" +
+        "donec vulputate dui habitant, consequat aenean rhoncus amet fames fringilla, ultrices velit sed fames.",
+      races: ["Human", "Orc", "Elf"],
+      classes: ["Cleric", "Bard", "Warrior"]
+    },
+    {
+      name: "star wars",
+      description: "asdasdasdasdasdasdasdasd",
+      races: ["Jedi", "Siff", "Drone"],
+      classes: ["Human", "Droid", "Warrior"]
+    }
+  ];
   constructor(private modalService: ModalService) {
     this.modalService.status().subscribe(val => (this.activate = val));
+    this.selectedTheme = this.themes[0];
   }
 
   ngOnInit() {}
+
+  selectValueHandle(event: any) {
+    let selectedValue = event.target.value;
+    this.selectedTheme = this.themes.filter(
+      theme => theme.name == selectedValue
+    )[0];
+  }
 
   close() {
     this.modalService.close();
