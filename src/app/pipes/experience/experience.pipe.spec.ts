2d133f741c1a95c94e30389c7bead2b703e7acfa
lgmagalhaes <lucasgsm88@gmail.com>
2018-09-04 17:06:45 -0300

    adjusted experience pipe tests

--- src/app/pipes/experience/experience.pipe.spec.ts
+++ src/app/pipes/experience/experience.pipe.spec.ts
@@ -2,177 +2,183 @@ import { ExperiencePipe } from "./experience.pipe";
 
 describe("experiencePipe", () => {
   const pipe = new ExperiencePipe();
+  let formated: string;
   it("create an instance", () => {
     expect(pipe).toBeTruthy();
   });
 
   it("should value 'NaN' return 0", () => {
-    let formated = pipe.transform(NaN);
+    formated = pipe.transform(NaN);
+    expect(formated).toEqual("0");
+  });
+
+  it("should value '' return 0", () => {
+    formated = pipe.transform("");
     expect(formated).toEqual("0");
   });
 
   it("should value 'undefined' return 0", () => {
-    let formated = pipe.transform(undefined);
+    formated = pipe.transform(undefined);
     expect(formated).toEqual("0");
   });
 
   it("should value 'kaaka' return 0", () => {
-    let formated = pipe.transform(undefined);
+    formated = pipe.transform(undefined);
     expect(formated).toEqual("0");
   });
 
   it("should value 'null' return 0", () => {
-    let formated = pipe.transform(null);
+    formated = pipe.transform(null);
     expect(formated).toEqual("0");
   });
 
   it("should value 1.5 return 1", () => {
-    let formated = pipe.transform(1.5);
+    formated = pipe.transform(1.5);
     expect(formated).toEqual("2");
   });
 
   it("should value 1.2 return 0", () => {
-    let formated = pipe.transform(1.2);
+    formated = pipe.transform(1.2);
     expect(formated).toEqual("1");
   });
 
   it("should value 1.6 return 0", () => {
-    let formated = pipe.transform(1.6);
+    formated = pipe.transform(1.6);
     expect(formated).toEqual("2");
   });
 
   it("should number 100 return 100", () => {
-    let formated = pipe.transform(100);
+    formated = pipe.transform(100);
     expect(formated).toEqual("100");
   });
 
   it("should string 100 return 100", () => {
-    let formated = pipe.transform("100");
+    formated = pipe.transform("100");
     expect(formated).toEqual("100");
   });
 
   it("should number 1000 return 1K", () => {
-    let formated = pipe.transform(1000);
+    formated = pipe.transform(1000);
     expect(formated).toEqual("1K");
   });
 
   it("should number 10000(10.000) return 10K", () => {
-    let formated = pipe.transform(10000);
+    formated = pipe.transform(10000);
     expect(formated).toEqual("10K");
   });
 
   it("should string 1000 return 1K", () => {
-    let formated = pipe.transform("1000");
+    formated = pipe.transform("1000");
     expect(formated).toEqual("1K");
   });
 
   it("should number 1001 return +1K", () => {
-    let formated = pipe.transform(1001);
+    formated = pipe.transform(1001);
     expect(formated).toEqual("+1K");
   });
 
   it("should string 1001 return +1K", () => {
-    let formated = pipe.transform("1001");
+    formated = pipe.transform("1001");
     expect(formated).toEqual("+1K");
   });
 
   it("should number 1000000 (1.000.000) return 1M", () => {
-    let formated = pipe.transform(1000000);
+    formated = pipe.transform(1000000);
     expect(formated).toEqual("1M");
   });
 
   it("should number 10000000 (10.000.000) return 10M", () => {
-    let formated = pipe.transform(10000000);
+    formated = pipe.transform(10000000);
     expect(formated).toEqual("10M");
   });
 
   it("should number 100000000 (100.000.000) return 100M", () => {
-    let formated = pipe.transform(100000000);
+    formated = pipe.transform(100000000);
     expect(formated).toEqual("100M");
   });
 
   it("should string 1000000 (1.000.000) return 1M", () => {
-    let formated = pipe.transform("1000000");
+    formated = pipe.transform("1000000");
     expect(formated).toEqual("1M");
   });
 
   it("should number 1000001 (1.000.001) return +1M", () => {
-    let formated = pipe.transform(1000001);
+    formated = pipe.transform(1000001);
     expect(formated).toEqual("+1M");
   });
 
   it("should string 1000001 (1.000.001) return +1M", () => {
-    let formated = pipe.transform("1000001");
+    formated = pipe.transform("1000001");
     expect(formated).toEqual("+1M");
   });
 
   it("should number 1000000000 (1.000.000.000) return 1B", () => {
-    let formated = pipe.transform(1000000000);
+    formated = pipe.transform(1000000000);
     expect(formated).toEqual("1B");
   });
 
   it("should number 10000000000 (10.000.000.000) return 10B", () => {
-    let formated = pipe.transform(10000000000);
+    formated = pipe.transform(10000000000);
     expect(formated).toEqual("10B");
   });
 
   it("should number 100000000000 (100.000.000.000) return 100B", () => {
-    let formated = pipe.transform(100000000000);
+    formated = pipe.transform(100000000000);
     expect(formated).toEqual("100B");
   });
 
   it("should string 1000000000 (1.000.000.000) return 1B", () => {
-    let formated = pipe.transform("1000000000");
+    formated = pipe.transform("1000000000");
     expect(formated).toEqual("1B");
   });
 
   it("should number 1000000001 (1.000.000.001) return +1B", () => {
-    let formated = pipe.transform(1000000001);
+    formated = pipe.transform(1000000001);
     expect(formated).toEqual("+1B");
   });
 
   it("should string 1000000001 (1.000.000.001) return +1B", () => {
-    let formated = pipe.transform("1000000001");
+    formated = pipe.transform("1000000001");
     expect(formated).toEqual("+1B");
   });
 
   it("should number 1000000000000 (1.000.000.000.000) return 1T", () => {
-    let formated = pipe.transform(1000000000000);
+    formated = pipe.transform(1000000000000);
     expect(formated).toEqual("1T");
   });
 
   it("should number 10000000000000 (10.000.000.000.000) return 10T", () => {
-    let formated = pipe.transform(10000000000000);
+    formated = pipe.transform(10000000000000);
     expect(formated).toEqual("10T");
   });
 
   it("should number 100000000000000 (100.000.000.000.000) return 100T", () => {
-    let formated = pipe.transform(100000000000000);
+    formated = pipe.transform(100000000000000);
     expect(formated).toEqual("100T");
   });
 
   it("should string 1000000000000 (1.000.000.000.000) return 1T", () => {
-    let formated = pipe.transform("1000000000000");
+    formated = pipe.transform("1000000000000");
     expect(formated).toEqual("1T");
   });
 
   it("should number 1000000000001 (1.000.000.000.001) return +1T", () => {
-    let formated = pipe.transform(1000000000001);
+    formated = pipe.transform(1000000000001);
     expect(formated).toEqual("+1T");
   });
 
   it("should string 1000000000001 (1.000.000.000.001) return +1T", () => {
-    let formated = pipe.transform("1000000000001");
+    formated = pipe.transform("1000000000001");
     expect(formated).toEqual("+1T");
   });
 
   it("should number 1000000000000000 (1.000.000.000.000.000) return ∞", () => {
-    let formated = pipe.transform(1000000000000000);
+    formated = pipe.transform(1000000000000000);
     expect(formated).toEqual("∞");
   });
 
   it("should string 1000000000000000 (1.000.000.000.000.000) return ∞", () => {
-    let formated = pipe.transform("1000000000000000");
+    formated = pipe.transform("1000000000000000");
     expect(formated).toEqual("∞");
   });
 });
